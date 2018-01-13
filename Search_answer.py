346bbdc91255cab1ff29b98744a0bb677cf816da
yyzhou94 <yyzhou@njnet.edu.cn>
2018-01-13 14:42:42 +0800

    Add files via upload

---
+++ Search_answer.py
@@ -0,0 +1,75 @@
+# -*- coding:utf-8 -*-
+import urllib.request,time,_thread,urllib.parse
+
+class Search:
+
+    def biggest(self,a,b,c):  #获取出现次数最多的答案
+         if a>b:
+             maxnum = a
+         else:
+             maxnum = b
+         if c>maxnum:
+             maxnum=c
+         return maxnum
+
+    def __init__(self,question,choice): # 注意前后各两个下划线
+        self.start = time.time()
+        self.question = question
+        self.choice = choice
+        self.a = 0
+        self.b = 0
+        self.c = 0
+        self.count = 0
+        
+    def gethtml(self,url):
+     headers = ('User-Agent','Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11')
+
+     opener = urllib.request.build_opener()
+     opener.addheaders = [headers]
+     date = opener.open(url).read()
+     
+     if "zhidao.baidu.com" in url:
+        str1=date.decode('gbk').encode('utf-8').decode('utf-8')
+     else:
+        str1=str(date,"utf-8")
+
+
+     self.count += 1
+     self.a += str1.count(self.choice[0].replace('A', ''))
+     self.b += str1.count(self.choice[1].replace('B', ''))
+     self.c += str1.count(self.choice[2].replace('C', ''))
+     
+    def threhtml(self,url):    #开线程获得网页
+        _thread.start_new_thread(self.gethtml,(url,))
+
+    def search(self):           #要搜索的引擎
+        
+		 #可以自己添加搜索接口  self.threhtml(网址)
+         baidu = self.threhtml("https://zhidao.baidu.com/search?lm=0&rn=10&pn=0&fr=search&ie=gbk&word="+urllib.parse.quote(self.question,encoding='gbk'))
+
+         sousou = self.threhtml("http://wenwen.sogou.com/s/?w="+urllib.parse.quote(self.question)+"&ch=ww.header.ssda")
+
+         iask = self.threhtml("https://iask.sina.com.cn/search?searchWord="+urllib.parse.quote(self.question)+"&record=1")
+
+         so360 = self.threhtml("https://wenda.so.com/search/?q="+urllib.parse.quote(self.question))
+
+    
+         while 1:
+           if(self.count == 4):
+               break
+
+         dict = {self.a: 'A', self.b: 'B', self.c: 'C'}
+
+         listselect = [self.a,self.b,self.c]
+         print('---------------------------------')
+         print(' 选项    出现次数  ')
+         print('  A：     ' + str(self.a))
+         print('  B：     ' + str(self.b))
+         print('  C：     ' + str(self.c))
+         print('---------------------------------')
+         print('  推荐答案(否定请选次数最小者)：' + dict[self.biggest(self.a,self.b,self.c)])
+         print('---------------------------------')
+         print()
+
+         end = time.time()
+         print('搜索用时：'+str(end-self.start)+'秒')
---
+++ assistant_test.py
@@ -0,0 +1,72 @@
+# -*- coding:utf-8 -*-
+
+import urllib.request,sys,base64,json,os,time
+from PIL import Image
+from aip import AipOcr
+from Search_answer import Search
+
+app_id = '10673492'                               #百度识别的appID,apikey,secretkey（填你自己的）
+api_key= 'Y7xqHn1L04tAAVSlcpbK0Pwr'
+secret_key= 'bG4Ip2NByQh4nnOwLUEW4USR8dwxjPD1'    #每天上限500次，应该也够用了
+
+def main():
+  start = time.time()
+  os.system("adb shell /system/bin/screencap -p /sdcard/screenshot.png") 
+  os.system("adb pull /sdcard/screenshot.png ")  
+
+
+  im = Image.open(r"./screenshot.png")   
+  img_size = im.size
+  w = im.size[0]
+  h = im.size[1]
+  print("xx:{}".format(img_size))
+
+  region = im.crop((70,200, w-70,1200))    #裁剪的区域
+  region.save("./crop_test1.png") 
+
+  f=open('./crop_test1.png','rb')
+  image_data=f.read()
+
+  client = AipOcr(app_id, api_key, secret_key)                #识别过程
+  result = client.basicGeneral(image_data)
+  if "error_code" in result:
+      print("baidu api error: ", result["error_msg"])
+  else:
+      words_results = result['words_result']                  #得到识别结果的问题+选项
+      #print (result['words_result'])   
+  question = ''
+  choice = ['','','','','','']
+  num = 0
+  choice_num = 0
+  for words_result in words_results:
+        num+=1
+        if(num >=len(words_results)-2):
+          choice[choice_num] = words_result['words']
+          choice_num+=1
+        else:
+          question = question + words_result['words']
+
+  
+  print(question)       #打印问题
+  print('  A:'+choice[0]+' B:'+choice[1]+' C:'+choice[2])       #打印选项
+
+  s = Search(question,choice)
+  s.search()
+  
+  end = time.time()
+  print('程序用时：'+str(end-start)+'秒')
+  
+while True:
+    print("""
+请在题目出现时按Enter搜索答案
+         """)
+
+    enter = input("按Enter键开始，按q键后Enter退出...")
+    if enter == 'q':
+        break
+    try:
+        main()
+    except Exception as e:
+        print(str(e))
+        
+print("欢迎下次使用,帮忙Git右上角点个star哦！")   
